0740882355aa6ee0fb5d62ffad2a319f63ebd0b6
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2024-03-16 16:04:57 +0000

    userの編集画面・機能の作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,17 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
   
   def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user)
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :icon_image)
   end
 end
