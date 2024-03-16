5bc8dbe1442a3f9282c4cc9ccf417e368527b0b6
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2024-03-16 20:00:57 +0000

    新規投稿機能を作成

--- app/controllers/gadgets_controller.rb
+++ app/controllers/gadgets_controller.rb
@@ -1,16 +1,27 @@
 class GadgetsController < ApplicationController
   def new
+    @gadget = Gadget.new
   end
   
   def create
+    gadget = Gadget.new(gadget_params)
+    gadget.user_id = current_user.id
+    gadget.save
+    redirect_to gadget_path(gadget)
   end
 
   def index
   end
 
   def show
+    @gadget = Gadget.find(params[:id])
   end
   
   def destroy
+  end
+  
+  private
+  def gadget_params
+    params.require(:gadget).permit(:title, :body, :gadget_image)
   end
 end
